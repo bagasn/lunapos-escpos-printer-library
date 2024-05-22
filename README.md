905cf7cdb397a4f91fbce508a3acc11f40ca4b3f
bagasn <bagasnasution@gmail.com>
2024-05-22 13:36:19 +0700

    Init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# rn-luna-escpos-printer
+
+## Getting started
+
+`$ npm install rn-luna-escpos-printer --save`
+
+### Mostly automatic installation
+
+`$ react-native link rn-luna-escpos-printer`
+
+## Usage
+```javascript
+import LunaEscposPrinter from 'rn-luna-escpos-printer';
+
+// TODO: What to do with the module?
+LunaEscposPrinter;
+```
---
+++ android/src/main/java/com/luna/escposprinter/LunaEscposPrinterModule.java
@@ -0,0 +1,29 @@
+// LunaEscposPrinterModule.java
+
+package com.luna.escposprinter;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class LunaEscposPrinterModule extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+
+    public LunaEscposPrinterModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "LunaEscposPrinter";
+    }
+
+    @ReactMethod
+    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
+        // TODO: Implement some actually useful functionality
+        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    }
+}
